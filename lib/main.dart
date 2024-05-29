88d72910296328bb6d3e7a2e8d9bc2e9873aca2c
Gerardo García Ortiz <gerardo.garcia@arkdiasolutions.com>
2024-05-28 23:54:39 -0600

    Se hizo un cambio por Gerardo

--- lib/main.dart
+++ lib/main.dart
@@ -3,12 +3,15 @@ import 'package:day15_shoes/Shoes.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(
-  MaterialApp(
-    debugShowCheckedModeBanner: false,
-    title: 'Nuevo Inicio',
-    home: HomePage(title: 'Rama AppBar', style: TextStyle(backgroundColor: Colors.blue),),
-  ),
-);
+      MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Cambios de Gerardo',
+        home: HomePage(
+          title: 'Rama AppBar',
+          style: TextStyle(backgroundColor: Colors.blue),
+        ),
+      ),
+    );
 
 class HomePage extends StatelessWidget {
   @override
@@ -18,15 +21,24 @@ class HomePage extends StatelessWidget {
         elevation: 0,
         backgroundColor: Colors.transparent,
         leading: null,
-        title: Text("Zapatos", style: TextStyle(color: Colors.black, fontSize: 25),),
+        title: Text(
+          "Zapatos",
+          style: TextStyle(color: Colors.black, fontSize: 25),
+        ),
         actions: <Widget>[
           IconButton(
             onPressed: () {},
-            icon: Icon(Icons.notifications_none, color: Colors.black,),
+            icon: Icon(
+              Icons.notifications_none,
+              color: Colors.black,
+            ),
           ),
           IconButton(
             onPressed: () {},
-            icon: Icon(Icons.Shoes, color: Colors.black,),
+            icon: Icon(
+              Icons.Shoes,
+              color: Colors.black,
+            ),
           )
         ],
       ),
@@ -41,61 +53,91 @@ class HomePage extends StatelessWidget {
                   scrollDirection: Axis.horizontal,
                   children: <Widget>[
                     AspectRatio(
-                      aspectRatio: 2.2/1,
-                      child: FadeInUp(duration: Duration(milliseconds: 1000), child: Container(
-                        margin: EdgeInsets.only(right: 10),
-                        decoration: BoxDecoration(
-                          color: Colors.grey[200],
-                          borderRadius: BorderRadius.circular(20)
-                        ),
-                        child: Center(
-                          child: Text("Todo", style: TextStyle(fontSize: 20),),
-                        ),
-                      )),
+                      aspectRatio: 2.2 / 1,
+                      child: FadeInUp(
+                          duration: Duration(milliseconds: 1000),
+                          child: Container(
+                            margin: EdgeInsets.only(right: 10),
+                            decoration: BoxDecoration(color: Colors.grey[200], borderRadius: BorderRadius.circular(20)),
+                            child: Center(
+                              child: Text(
+                                "Todo",
+                                style: TextStyle(fontSize: 20),
+                              ),
+                            ),
+                          )),
                     ),
                     AspectRatio(
-                      aspectRatio: 2.2/1,
-                      child: FadeInUp(duration: Duration(milliseconds: 1100), child: Container(
-                        margin: EdgeInsets.only(right: 10),
-                        child: Center(
-                          child: Text("Tenis", style: TextStyle(fontSize: 17),),
-                        ),
-                      )),
+                      aspectRatio: 2.2 / 1,
+                      child: FadeInUp(
+                          duration: Duration(milliseconds: 1100),
+                          child: Container(
+                            margin: EdgeInsets.only(right: 10),
+                            child: Center(
+                              child: Text(
+                                "Tenis",
+                                style: TextStyle(fontSize: 17),
+                              ),
+                            ),
+                          )),
                     ),
                     AspectRatio(
-                      aspectRatio: 2.2/1,
-                      child: FadeInUp(duration: Duration(milliseconds: 1200), child: Container(
-                        margin: EdgeInsets.only(right: 10),
-                        child: Center(
-                          child: Text("Futbol Americano", style: TextStyle(fontSize: 17),),
-                        ),
-                      )),
+                      aspectRatio: 2.2 / 1,
+                      child: FadeInUp(
+                          duration: Duration(milliseconds: 1200),
+                          child: Container(
+                            margin: EdgeInsets.only(right: 10),
+                            child: Center(
+                              child: Text(
+                                "Futbol Americano",
+                                style: TextStyle(fontSize: 17),
+                              ),
+                            ),
+                          )),
                     ),
                     AspectRatio(
-                      aspectRatio: 2.2/1,
-                      child: FadeInUp(duration: Duration(milliseconds: 1300), child: Container(
-                        margin: EdgeInsets.only(right: 10),
-                        child: Center(
-                          child: Text("Soccer", style: TextStyle(fontSize: 17),),
-                        ),
-                      )),
+                      aspectRatio: 2.2 / 1,
+                      child: FadeInUp(
+                          duration: Duration(milliseconds: 1300),
+                          child: Container(
+                            margin: EdgeInsets.only(right: 10),
+                            child: Center(
+                              child: Text(
+                                "Soccer",
+                                style: TextStyle(fontSize: 17),
+                              ),
+                            ),
+                          )),
                     ),
                     AspectRatio(
-                      aspectRatio: 2.2/1,
-                      child: FadeInUp(duration: Duration(milliseconds: 1400), child: Container(
-                        margin: EdgeInsets.only(right: 10),
-                        child: Center(
-                          child: Text("Golf Juego", style: TextStyle(fontSize: 17),),
-                        ),
-                      )),
+                      aspectRatio: 2.2 / 1,
+                      child: FadeInUp(
+                          duration: Duration(milliseconds: 1400),
+                          child: Container(
+                            margin: EdgeInsets.only(right: 10),
+                            child: Center(
+                              child: Text(
+                                "Golf Juego",
+                                style: TextStyle(fontSize: 17),
+                              ),
+                            ),
+                          )),
                     ),
                   ],
                 ),
               ),
-              SizedBox(height: 20,),
-              FadeInUp(duration: Duration(milliseconds: 1500), child: makeItem(image: 'assets/images/one.jpg', tag: 'red', context: context)),
-              FadeInUp(duration: Duration(milliseconds: 1600), child: makeItem(image: 'assets/images/two.jpg', tag: 'blue', context: context)),
-              FadeInUp(duration: Duration(milliseconds: 1700), child: makeItem(image: 'assets/images/three.jpg', tag: 'white', context: context)),
+              SizedBox(
+                height: 20,
+              ),
+              FadeInUp(
+                  duration: Duration(milliseconds: 1500),
+                  child: makeItem(image: 'assets/images/one.jpg', tag: 'red', context: context)),
+              FadeInUp(
+                  duration: Duration(milliseconds: 1600),
+                  child: makeItem(image: 'assets/images/two.jpg', tag: 'blue', context: context)),
+              FadeInUp(
+                  duration: Duration(milliseconds: 1700),
+                  child: makeItem(image: 'assets/images/three.jpg', tag: 'white', context: context)),
             ],
           ),
         ),
@@ -108,7 +150,13 @@ class HomePage extends StatelessWidget {
       tag: tag,
       child: GestureDetector(
         onTap: () {
-          Navigator.push(context, MaterialPageRoute(builder: (context) => Shoes(image: image, tag: tag,)));
+          Navigator.push(
+              context,
+              MaterialPageRoute(
+                  builder: (context) => Shoes(
+                        image: image,
+                        tag: tag,
+                      )));
         },
         child: Container(
           height: 250,
@@ -116,19 +164,9 @@ class HomePage extends StatelessWidget {
           padding: EdgeInsets.all(20),
           margin: EdgeInsets.only(bottom: 20),
           decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(20),
-            image: DecorationImage(
-              image: AssetImage(image),
-              fit: BoxFit.cover
-            ),
-            boxShadow: [
-              BoxShadow(
-                color: Colors.grey.shade400,
-                blurRadius: 10,
-                offset: Offset(0, 10)
-              )
-            ]
-          ),
+              borderRadius: BorderRadius.circular(20),
+              image: DecorationImage(image: AssetImage(image), fit: BoxFit.cover),
+              boxShadow: [BoxShadow(color: Colors.grey.shade400, blurRadius: 10, offset: Offset(0, 10))]),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -140,27 +178,45 @@ class HomePage extends StatelessWidget {
                     child: Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: <Widget>[
-                        FadeInUp(duration: Duration(milliseconds: 1000), child: Text("Sneakers", style: TextStyle(color: Colors.white, fontSize: 30, fontWeight: FontWeight.bold),)),
-                        SizedBox(height: 10,),
-                        FadeInUp(duration: Duration(milliseconds: 1100), child: Text("Nike", style: TextStyle(color: Colors.white, fontSize: 20),)),
-
+                        FadeInUp(
+                            duration: Duration(milliseconds: 1000),
+                            child: Text(
+                              "Sneakers",
+                              style: TextStyle(color: Colors.white, fontSize: 30, fontWeight: FontWeight.bold),
+                            )),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        FadeInUp(
+                            duration: Duration(milliseconds: 1100),
+                            child: Text(
+                              "Nike",
+                              style: TextStyle(color: Colors.white, fontSize: 20),
+                            )),
                       ],
                     ),
                   ),
-                  FadeInUp(duration: Duration(milliseconds: 1200), child: Container(
-                    width: 35,
-                    height: 35,
-                    decoration: BoxDecoration(
-                      shape: BoxShape.circle,
-                      color: Colors.white
-                    ),
-                    child: Center(
-                      child: Icon(Icons.favorite_border, size: 20,),
-                    ),
-                  ))
+                  FadeInUp(
+                      duration: Duration(milliseconds: 1200),
+                      child: Container(
+                        width: 35,
+                        height: 35,
+                        decoration: BoxDecoration(shape: BoxShape.circle, color: Colors.white),
+                        child: Center(
+                          child: Icon(
+                            Icons.favorite_border,
+                            size: 20,
+                          ),
+                        ),
+                      ))
                 ],
               ),
-              FadeInUp(duration: Duration(milliseconds: 1200), child: Text("100\$", style: TextStyle(color: Colors.white, fontSize: 30, fontWeight: FontWeight.bold),)),              
+              FadeInUp(
+                  duration: Duration(milliseconds: 1200),
+                  child: Text(
+                    "100\$",
+                    style: TextStyle(color: Colors.white, fontSize: 30, fontWeight: FontWeight.bold),
+                  )),
             ],
           ),
         ),
